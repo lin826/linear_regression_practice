4619744ff541b6467beb5bbe5732a9ff0a8344ba
lin826 <liniju826@gmail.com>
2017-03-21 19:40:32 +0800

    Add ouput to csv

--- data_io.py
+++ data_io.py
@@ -1,10 +1,6 @@
 import csv
 def get_train_data(x_file,t_file):
-    with open(x_file, 'r') as locCsvfile:
-        loc_reader = csv.reader(locCsvfile)
-        train_loc = []
-        for row in loc_reader:
-            train_loc.append([int(iterator) for iterator in row])
+    train_loc = get_loc_data(x_file)
     with open(t_file, 'r') as valCsvfile:
         val_reader = csv.reader(valCsvfile)
         train_val = []
@@ -12,8 +8,10 @@ def get_train_data(x_file,t_file):
             train_val.append([int(row[0])])
     return train_loc, train_val # Need to convert to np.array()
 
-def output_test_result(t_file,y):
-    with open(t_file, 'wb') as csvfile:
-        writer = csv.DictWriter(csvfile)
-        for e in y:
-            writer.writerow(e)
+def get_loc_data(x_file):
+    with open(x_file, 'r') as locCsvfile:
+        loc_reader = csv.reader(locCsvfile)
+        train_loc = []
+        for row in loc_reader:
+            train_loc.append([int(iterator) for iterator in row])
+        return train_loc
--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from approach import *
 APPR = Test_approach
 
 # Parameters settings
-settings = {"map_size":1081,"dim":2,"data_size":10000,
-    "batch_size":1,"iter":10,"eta":0.8,"k_folder":0,
+settings = {"map_size":1081,"dim":2,"data_size":32000,
+    "batch_size":1,"iter":100,"eta":0.5,"k_folder":0,
     "basis_size":1024,"sigma":30, "lamb":0.05,
     "m0":0,"alpha":2,"beta":25,"graph_scale":2,
     "x_train":"data/X_train.csv","t_train":"data/T_train.csv"}
@@ -40,6 +40,17 @@ def set_approach(data):
     else:
         print("No approach:",data)
 
+def try_test(x_file,w):
+    test_x = get_loc_data(x_file)
+    graph_x, graph_t = get_graph_data(w)
+    result = []
+    with open('data/'+str(APPR)+'.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';', quotechar="'", quoting=csv.QUOTE_ALL)
+        for i in range(len(test_x)):
+            for j in range(len(graph_x)):
+                if graph_x[j][0] == test_x[i][0] and graph_x[j][1] == test_x[i][1] :
+                    writer.writerow([graph_t[j]])
+                    print(test_x[i],graph_t[j])
 
 # Main() where the start of program
 if __name__ == "__main__":
@@ -49,11 +60,13 @@ if __name__ == "__main__":
             opt = arg[i][2:]
             main_opt(opt,arg[i+1])
     model_init(settings)
+
     for k in range(settings['k_folder']+1):
         if model_setting(settings,k+1) < 0:
             break
         set_Gausian_basis()
         weights = APPR()
-    print(weights)
+    # print(weights)
     cal_MSE(weights)
-    draw(weights,show_2d_gragh)
+    # draw(weights,show_2d_gragh)
+    # try_test('data/X_test.csv',weights)
